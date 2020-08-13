1b8ebf090672c91c58a1fbf68838958e29eb515c
JoelMendozaP <joelmendozapaucara@gmail.com>
2020-08-13 19:11:19 -0400

    dasd

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=198.199.122.50;dbname=kardex",
+		$link = new PDO("mysql:host=198.199.122.50;ports=3306;dbname=kardex",
 			            "root",
 			            "12345678");
 
